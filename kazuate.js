ae39c689243f90f4245fadf05ecb104dbc559a43
G285082022 <228508@st.takushoku-u.ac.jp>
2023-07-06 16:05:37 +0900

    フォルダ修正

--- kazuate.js
+++ kazuate.js
@@ -8,18 +8,20 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
+let kaito = document.querySelector('button#kaito');
+kaito.addEventListener('click', hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
     let sKaisu = document.querySelector('span#kaisu');
     let sAnswer = document.querySelector('span#answer');
     let pResult = document.querySelector('p#result');
+    let iYoso = document.querySelector('input[name=yoso]');
 
     kaisu++;
 
     // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-    let yoso = 4;
+    let yoso = parseInt(iYoso.value);
 
     sKaisu.textContent = kaisu;
     sAnswer.textContent = yoso;
